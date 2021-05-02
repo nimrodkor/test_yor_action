307bebd8da14744f671bd3599c8caa2e7b501254
nimrodkor <nimrodkor@gmail.com>
2021-05-02 12:25:58 +0300

    Revert tags

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,8 @@
 resource "aws_s3_bucket" "my_bucket" {
   name = "bucket1"
-  tags = {
-    yor_trace = "c1ad066a-87ba-4cf1-b015-5e18c0d159a6"
-  }
 }
 
 resource "aws_instance" "my_instance" {
   ami           = "some-ami"
   instance_type = "t3.large"
-  tags = {
-    yor_trace = "c261c37d-6b5f-4531-8160-d3fbc6508e9e"
-  }
 }
